dbb1fe232691b2e24c7827ac845c9f842f13ca07
Rezan92 <rezanibrahim8@gmail.com>
2020-09-10 16:33:28 +0200

    Cleand the code

--- Week2/homework/pokemon-app/script.js
+++ Week2/homework/pokemon-app/script.js
@@ -1,53 +1,53 @@
 function main() {
-  let pokemons;
+  const URL = "https://pokeapi.co/api/v2/pokemon?limit=150";
   const body = document.body;
-  const container = document.createElement("div")
+  const container = document.createElement("div");
   const btnGetPokemon = document.createElement("button");
-  const btnSelectPokemon = document.createElement("select");
+  const selectPokemon = document.createElement("select");
   const pokemonImage1 = document.createElement("img");
   const pokemonImage2 = document.createElement("img");
 
   btnGetPokemon.textContent = "Get Pokemon";
   container.classList.add("container", "mt-5", "bg-dark");
   btnGetPokemon.classList.add("btn", "btn-success");
-  btnSelectPokemon.classList.add("d-block", "btn", "btn-success", "mt-3");
+  selectPokemon.classList.add("d-block", "btn", "btn-success", "mt-3");
   pokemonImage1.classList.add("w-25", "mt-3");
   pokemonImage2.classList.add("w-25", "mt-3");
 
   body.appendChild(container);
   container.appendChild(btnGetPokemon);
-  container.appendChild(btnSelectPokemon);
+  container.appendChild(selectPokemon);
   container.appendChild(pokemonImage1);
   container.appendChild(pokemonImage2);
 
-  btnGetPokemon.addEventListener("click", fetchData);
-  btnSelectPokemon.addEventListener("change", addPokemonToDOM);
+  btnGetPokemon.addEventListener("click", () => fetchData(URL, displayPokemonName));
+  selectPokemon.addEventListener("change", () => fetchData(URL, displayPokemonImage));
 
-  function fetchData() {
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=150")
+  function fetchData(url, callback) {
+    fetch(url)
       .then(res => res.json())
-      .then(data => {
-        pokemons = data.results;
-        pokemons.forEach((pokemon, i) => {
-          btnSelectPokemon.innerHTML += `<option>${i + 1}-${pokemon.name}</option>`;
-        });
-      })
+      .then(data => data.results ? callback(data.results) : callback(data))
       .catch(error => console.log(error))
   };
 
-  function addPokemonToDOM() {
-    pokemons.forEach((pokemon, i) => {
-      if (`${i + 1}-${pokemon.name}` === btnSelectPokemon.value) {
-        fetch(pokemon.url)
-          .then(res => res.json())
-          .then(data => {
-            pokemonImage1.src = data.sprites.back_default;
-            pokemonImage2.src = data.sprites.front_default;
-          })
-          .catch(error => console.log(error))
-      }
+  function displayPokemonName(array) {
+    array.forEach((pokemon, i) => {
+      selectPokemon.innerHTML += `<option>${i + 1}-${pokemon.name}</option>`;
     })
-  }
+  };
+
+  function displayPokemonImage(array) {
+    array.forEach((pokemon, i) => {
+      if (`${i + 1}-${pokemon.name}` === selectPokemon.value) {
+        fetchData(pokemon.url, addImage);
+      }
+    });
+  };
+
+  function addImage(data) {
+    pokemonImage1.src = data.sprites.back_default;
+    pokemonImage2.src = data.sprites.front_default;
+  };
 };
 
-main();
+window.addEventListener("load", main());
